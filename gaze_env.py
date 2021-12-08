2895c9391b6e729204f11fe936bc8435e85a9469
Naoki Yokoyama <naokiyokoyama@fb.com>
2021-12-08 07:33:12 -0500

    working real env for Gaze task

---
+++ gaze_env.py
@@ -0,0 +1,160 @@
+import cv2
+import gym
+from gym import spaces
+import numpy as np
+import time
+
+from bd_spot_wrapper.spot import (
+    Spot,
+    SpotCamIds,
+    image_response_to_cv2,
+    scale_depth_img,
+)
+from bd_spot_wrapper.utils import color_bbox, say
+
+CTRL_HZ = 1.0
+INITIAL_ARM_JOINT_ANGLES = np.deg2rad([0, -150, 120, 0, 75, 0])
+JOINT_BLACKLIST = ["arm0.el0", "arm0.wr1"]  # joints we can't control
+MAX_EPISODE_STEPS = 120
+OBJECT_LOCK_ON_NEEDED = 3
+CENTER_TOLERANCE = 0.25
+ACTUALLY_GRASP = True
+ACTUALLY_MOVE_ARM = True
+MAX_JOINT_MOVEMENT = 0.0698132
+
+
+def pad_action(action):
+    """We only control 4 out of 6 joints; add zeros to non-controllable indices."""
+    return np.array([*action[:3], 0.0, action[3], 0.0])
+
+
+def scale_action(action):
+    return np.clip(action, -1.0, 1.0) * MAX_JOINT_MOVEMENT
+
+
+class SpotGazeEnv(gym.Env):
+    def __init__(self, spot: Spot):
+        # Standard Gym stuff
+        self.observation_space = spaces.Dict()
+        self.action_space = spaces.Dict()
+
+        # Arrange Spot into initial configuration
+        assert spot.spot_lease is not None, "Need motor control of Spot!"
+        spot.power_on()
+        say("Standing up")
+        spot.blocking_stand()
+        spot.open_gripper()
+
+        self.spot = spot
+        self.locked_on_object_count = 0
+        self.num_steps = 0
+        self.current_positions = np.zeros(6)
+        self.reset_ran = False
+
+    def reset(self):
+        # Move arm to initial configuration
+        self.spot.open_gripper()
+        cmd_id = self.spot.set_arm_joint_positions(
+            positions=INITIAL_ARM_JOINT_ANGLES, travel_time=2
+        )
+        self.spot.block_until_arm_arrives(cmd_id, timeout_sec=2)
+
+        # Reset parameters
+        self.locked_on_object_count = 0
+        self.num_steps = 0
+        self.reset_ran = True
+
+        observations = self.get_observations()
+        return observations
+
+    def step(self, action):
+        """
+        Moves the arm and returns updated observations
+        :param action: np.array of radians denoting how much each joint is to be moved
+        :return:
+        """
+        assert self.reset_ran, ".reset() must be called first!"
+
+        # Move the arm
+        padded_action = pad_action(action)  # insert zeros for joints we don't control
+        padded_action = scale_action(padded_action)
+        target_positions = np.clip(
+            self.current_positions + padded_action, -np.pi, np.pi
+        )
+        if ACTUALLY_MOVE_ARM:
+            _ = self.spot.set_arm_joint_positions(
+                positions=target_positions, travel_time=1 / CTRL_HZ
+            )
+
+        # Get observation
+        time.sleep(1 / CTRL_HZ)
+        observations = self.get_observations()
+
+        # Grasp object if conditions are met
+        grasp = self.locked_on_object_count == OBJECT_LOCK_ON_NEEDED
+        if grasp:
+            say("Locked on, executing auto grasp")
+            self.spot.loginfo("Conditions for auto-grasp have been met!")
+            if ACTUALLY_GRASP:
+                # Grab whatever object is at the center of hand RGB camera image
+                self.spot.grasp_center_of_hand_depth()
+                # Return to pre-grasp joint positions
+                cmd_id = self.spot.set_arm_joint_positions(
+                    positions=self.current_positions, travel_time=1.0
+                )
+                self.spot.block_until_arm_arrives(cmd_id, timeout_sec=2)
+            else:
+                time.sleep(5)
+
+        done = self.num_steps == MAX_EPISODE_STEPS or grasp
+
+        # Don't need reward or info
+        reward, info = None, {}
+        return observations, reward, done, info
+
+    def get_observations(self):
+        # Get proprioception inputs
+        arm_proprioception = self.spot.get_arm_proprioception()
+        self.current_positions = np.array(
+            [v.position.value for v in arm_proprioception.values()]
+        )
+        joints = np.array(
+            [
+                v.position.value
+                for v in arm_proprioception.values()
+                if v.name not in JOINT_BLACKLIST
+            ],
+            dtype=np.float32,
+        )
+        assert len(joints) == 4
+
+        # Get visual inputs
+        image_responses = self.spot.get_image_responses(
+            [SpotCamIds.HAND_COLOR, SpotCamIds.HAND_DEPTH_IN_HAND_COLOR_FRAME]
+        )
+        arm_rgb, raw_arm_depth = [image_response_to_cv2(i) for i in image_responses]
+        arm_depth = scale_depth_img(raw_arm_depth, max_depth=10.0)
+        arm_rgb, arm_depth = [cv2.resize(i, (320, 240)) for i in [arm_rgb, arm_depth]]
+        arm_depth = arm_depth.reshape([*arm_depth.shape, 1])  # unsqueeze
+        arm_depth_bbox, cx, cy, crosshair_in_bbox = color_bbox(arm_rgb)
+        locked_on = crosshair_in_bbox and all(
+            [abs(c - 0.5) < CENTER_TOLERANCE for c in [cx, cy]]
+        )
+        if locked_on:
+            self.locked_on_object_count += 1
+            self.spot.loginfo(
+                f"Locked on to target {self.locked_on_object_count} time(s)..."
+            )
+        else:
+            if self.locked_on_object_count > 0:
+                self.spot.loginfo("Lost lock-on!")
+            self.locked_on_object_count = 0
+
+        observations = {
+            "joint": joints,
+            "is_holding": np.zeros(1, dtype=np.float32),
+            "arm_depth": arm_depth,
+            "arm_depth_bbox": arm_depth_bbox,
+        }
+
+        return observations
---
+++ gaze_policy.py
@@ -0,0 +1,121 @@
+from habitat_baselines.rl.ppo.policy import PointNavBaselinePolicy
+from habitat_baselines.utils.common import batch_obs
+from habitat.core.spaces import ActionSpace
+
+import cv2
+from gym import spaces
+from gym.spaces import Dict as SpaceDict
+import torch
+import numpy as np
+
+# Turn numpy observations into torch tensors for consumption by policy
+def to_tensor(v):
+    if torch.is_tensor(v):
+        return v
+    elif isinstance(v, np.ndarray):
+        return torch.from_numpy(v)
+    else:
+        return torch.tensor(v, dtype=torch.float)
+
+
+class RealPolicy:
+    def __init__(self, checkpoint_path, observation_space, action_space, device):
+        self.device = device
+
+        checkpoint = torch.load(checkpoint_path, map_location="cpu")
+
+        config = checkpoint["config"]
+
+        """ Disable observation transforms for real world experiments """
+        config.defrost()
+        config.RL.POLICY.OBS_TRANSFORMS.ENABLED_TRANSFORMS = []
+        config.freeze()
+
+        self.policy = PointNavBaselinePolicy.from_config(
+            config=config,
+            observation_space=observation_space,
+            action_space=action_space,
+        )
+
+        # Move it to the device
+        self.policy.to(self.device)
+
+        # Load trained weights into the policy
+        self.policy.load_state_dict(
+            {k[len("actor_critic.") :]: v for k, v in checkpoint["state_dict"].items()}
+        )
+
+        self.prev_actions = None
+        self.test_recurrent_hidden_states = None
+        self.not_done_masks = None
+        self.config = config
+        self.num_actions = action_space.shape[0]
+        self.reset_ran = False
+
+    def reset(self):
+        self.reset_ran = True
+        self.test_recurrent_hidden_states = torch.zeros(
+            1,  # The number of environments. Just one for real world.
+            self.policy.net.num_recurrent_layers,
+            self.config.RL.PPO.hidden_size,
+            device=self.device,
+        )
+
+        # We start an episode with 'done' being True (0 for 'not_done')
+        self.not_done_masks = torch.zeros(1, 1, dtype=torch.bool, device=self.device)
+        self.prev_actions = torch.zeros(1, self.num_actions, device=self.device)
+
+    def act(self, observations):
+        assert self.reset_ran, "You need to call .reset() on the policy first."
+        batch = batch_obs([observations], device=self.device)
+        with torch.no_grad():
+            _, actions, _, self.test_recurrent_hidden_states = self.policy.act(
+                batch,
+                self.test_recurrent_hidden_states,
+                self.prev_actions,
+                self.not_done_masks,
+                deterministic=True,
+            )
+        self.prev_actions.copy_(actions)
+        self.not_done_masks = torch.ones(1, 1, dtype=torch.bool, device=self.device)
+
+        # GPU/CPU torch tensor -> numpy
+        actions = actions.squeeze().cpu().numpy()
+
+        return actions
+
+
+class GazePolicy(RealPolicy):
+    def __init__(self, checkpoint_path, device):
+        observation_space = SpaceDict(
+            {
+                "arm_depth": spaces.Box(
+                    low=0.0, high=1.0, shape=(240, 320, 1), dtype=np.float32
+                ),
+                "arm_depth_bbox": spaces.Box(
+                    low=0.0, high=1.0, shape=(240, 320, 1), dtype=np.float32
+                ),
+                "joint": spaces.Box(low=0.0, high=1.0, shape=(4,), dtype=np.float32),
+                "is_holding": spaces.Box(
+                    low=0.0, high=1.0, shape=(1,), dtype=np.float32
+                ),
+            }
+        )
+        action_space = spaces.Box(-1.0, 1.0, (4,))
+        super().__init__(checkpoint_path, observation_space, action_space, device)
+
+
+if __name__ == "__main__":
+    gaze_policy = GazePolicy(
+        "/Users/naokiyokoyama/gt/spot/spot_rl_experiments/bbox_mask_5thresh_autograsp_shortrange_seed1_36.pth",
+        device="cpu",
+    )
+    gaze_policy.reset()
+    observations = {
+        "arm_depth": np.zeros([240, 320, 1], dtype=np.float32),
+        "arm_depth_bbox": np.zeros([240, 320, 1], dtype=np.float32),
+        "joint": np.zeros(4, dtype=np.float32),
+        "is_holding": np.zeros(1, dtype=np.float32),
+    }
+    actions = gaze_policy.act(observations)
+    print("actions:", actions)
