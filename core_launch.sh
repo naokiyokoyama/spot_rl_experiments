5b31dbdcb1d6dd78c0f348c64e3a28d00d9ed8a0
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-04-12 11:28:10 -0400

    added headless_estop node

--- core_launch.sh
+++ core_launch.sh
@@ -2,10 +2,13 @@ conda activate spot_ros
 echo "Killing all tmux sessions..."
 tmux kill-server
 sleep 2
-echo "Starting roscore"
+echo "Starting roscore tmux..."
 tmux new -s roscore -d '/home/spot/miniconda3/envs/spot_ros/bin/roscore'
-sleep 3
-echo "Starting nodes"
+sleep 1
+echo "Starting other tmux nodes"
+tmux new -s headless_estop -d '$CONDA_PREFIX/bin/python -m spot_wrapper.headless_estop'
 tmux new -s img_pub -d '$CONDA_PREFIX/bin/python -m spot_rl.spot_ros_node'
 tmux new -s propio_pub -d '$CONDA_PREFIX/bin/python -m spot_rl.spot_ros_node -p'
 tmux new -s tts_sub -d '$CONDA_PREFIX/bin/python -m spot_rl.spot_ros_node -t'
+sleep 3
+tmux ls
